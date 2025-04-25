471c182141ed6e7650531842634b7b6586630dbc
HeshamRagab <106036527+HeshamRagab@users.noreply.github.com>
2025-04-26 02:47:42 +0300

    Time: 0 ms (100%), Space: 9.4 MB (25.69%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    unordered_map<char, string> t;
-    string gen = "";
-    vector<string> letterCombinations(string digits) {
-    	if(digits.size() == 0)
-    	{
-    		return {};
-    	}
-    t['2'] = "abc";
-    t['3'] = "def";
-    t['4'] = "ghi";
-    t['5'] = "jkl";
-    t['6'] = "mno";
-    t['7'] = "pqrs";
-    t['8'] = "tuv";
-    t['9'] = "wxyz";
-    vector<string> ans;
-    solve(0, digits, ans);
-    return ans;
+    vector<string> combinations;
+    string combination = "";
+    string t[3] = {"pqrs", "tuv", "wxyz"};
+    void solve(int i, const string& digits){
+        if(i >= digits.size())
+        {
+            if(combination.size())
+                combinations.emplace_back(combination);
+            return;
+        }
+        int d = digits[i] - '0';
+        d -= 1;
+        d *= 3;
+        //debug(i, d);
+        if(digits[i] > '6')
+        {
+            for(auto ch: t[digits[i] - '7'])
+            {
+                combination += ch;
+                solve(i + 1, digits);
+                combination.pop_back();
+            }
+        }
+        else
+        {
+            for(char c = 3; c > 0; c--){
+                combination += ('a' + (d - c));
+                solve(i + 1, digits);
+                combination.pop_back();
+            }
+        }
     }
-    void solve(int i, const string& d, vector<string>& ans){
-    	if(gen.size() >= d.size())
-    	{
-    		//cout << gen << " ";
-    		ans.push_back(gen);
-    		return;
-    	}
-    	for(auto ch: t[d[i]])
-    	{
-    		gen += ch;
-    		solve(i+1, d, ans);
-    		gen.pop_back();
-    	}
+    vector<string> letterCombinations(string digits) {
+        solve(0, digits);
+        return combinations;
     }
 };
